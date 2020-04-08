69d770b5cc5784b4f6dbc317e785fde7c38eb09e
Lukas <lukas@klusis.com>
2020-04-08 22:29:13 +0300

    Convert githunt shcema to SDL

--- packages/graphql-codegen-types-map/src/test-support/schema/githunt/index.ts
+++ packages/graphql-codegen-types-map/src/test-support/schema/githunt/index.ts
@@ -1,12 +1,13 @@
-import schema from './schema.json'
-import fs from 'fs'
-import path from 'path'
-import glob from 'glob'
 import { Types } from '@graphql-codegen/plugin-helpers'
-import { parse, buildClientSchema, IntrospectionQuery } from 'graphql'
+import fs from 'fs'
+import glob from 'glob'
+import { parse } from 'graphql'
+import path from 'path'
+
+import { schema } from './schema'
 
 const documents = glob
   .sync(path.join(__dirname, '*.graphql'))
   .map<Types.DocumentFile>(p => ({ location: p, document: parse(fs.readFileSync(p, 'utf8')) }))
 
-export const gitHuntSchema = { schema: buildClientSchema((schema as any) as IntrospectionQuery), documents }
+export const gitHuntSchema = { schema, documents }
